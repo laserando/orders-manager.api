d7be6492d3fcea7df76030ad829a838d12ff7f8e
R8-torlo <rubentorli.rt@gmail.com>
2021-07-22 15:56:53 +0200

    init

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '3b1841f02bb013ed6d4c858f59005d89'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'd9a102d3-0e32-469d-bea9-6bdcd5e7ab84'
+};
